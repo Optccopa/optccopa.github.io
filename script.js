cf8de2b08a27b315ff2fbd16f57207609617d7dc
Copa! <ryder4est@gmail.com>
2026-01-06 23:15:58 -0800

    test

--- script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const button = document.getElementById("alertBtn");
-
-  button.addEventListener("click", () => {
-    alert("Button clicked!");
-  });
-});
